298ab62d4a7e3264bcf4cb4e3f12751826be0e56
Igor Torquato <igor.torquato.20@gmail.com>
2023-05-23 13:14:15 -0300

    att atv12

--- 21_Aula16/atv12.c
+++ 21_Aula16/atv12.c
@@ -7,8 +7,11 @@
 #define MAX 100
 
 float *gerar_vetor(int n);
+float calcular_media(float *vetor, int n);
+float calcular_desvio_padrao(float *vetor, int n, float media);
 
 int main() {
+
     // Iniciando MPI
     MPI_Init(NULL, NULL);
     int nprocs;
@@ -16,62 +19,48 @@ int main() {
     int rank;
     MPI_Comm_rank(MPI_COMM_WORLD, &rank);
     MPI_Status status;
-
+    
     time_t t;
     srand(time(NULL) * rank);
-
-    int n = MAX; // Tamanho do vetor
+    
+    int n = MAX / nprocs; // Tamanho do subvetor em cada processo
     float *vetor;
-
-    /*
-    INICIO
-    */
+    
     if (rank == 0) {
-        vetor = gerar_vetor(n);
-
-        // Enviar o vetor para todos os processos
-        for (int dest = 1; dest < nprocs; dest++) {
-            MPI_Send(vetor, n, MPI_FLOAT, dest, 0, MPI_COMM_WORLD);
+        vetor = gerar_vetor(MAX);
+        // Imprime o vetor gerado
+        printf("Vetor gerado:\n");
+        for (int i = 0; i < MAX; i++) {
+            printf("%.2f ", vetor[i]);
         }
-    } else {
-        // Receber o vetor no processo atual
-        vetor = (float *)malloc(sizeof(float) * n);
-        MPI_Recv(vetor, n, MPI_FLOAT, 0, 0, MPI_COMM_WORLD, &status);
+        printf("\n\n");
     }
-
-    // Calcular a média do vetor localmente em cada processo
-    float soma_local = 0.0;
-    for (int i = 0; i < n; i++) {
-        soma_local += vetor[i];
-    }
-    float media_local = soma_local / n;
-
-    // Calcular a soma das médias de todos os processos
-    float soma_medias = 0.0;
-    MPI_Reduce(&media_local, &soma_medias, 1, MPI_FLOAT, MPI_SUM, 0, MPI_COMM_WORLD);
-
-    // Calcular o desvio padrão global
-    float desvio_local = 0.0;
-    for (int i = 0; i < n; i++) {
-        desvio_local += pow(vetor[i] - media_local, 2);
-    }
-    float desvio_padrao_local = sqrt(desvio_local / n);
+    
+    // Scatter do vetor para todos os processos
+    float *subvetor = (float *)malloc(sizeof(float) * n);
+    MPI_Scatter(vetor, n, MPI_FLOAT, subvetor, n, MPI_FLOAT, 0, MPI_COMM_WORLD);
+    
+    // Cálculo da média local em cada processo
+    float media_local = calcular_media(subvetor, n);
+    
+    // Cálculo da média global utilizando MPI_Allreduce
+    float media_global;
+    MPI_Allreduce(&media_local, &media_global, 1, MPI_FLOAT, MPI_SUM, MPI_COMM_WORLD);
+    media_global /= nprocs;
+    
+    // Cálculo do desvio padrão local em cada processo
+    float desvio_padrao_local = calcular_desvio_padrao(subvetor, n, media_global);
+    
+    // Cálculo do desvio padrão global utilizando MPI_Reduce
     float desvio_padrao_global;
-
-    // Calcular o desvio padrão global somando os desvios locais em todos os processos
     MPI_Reduce(&desvio_padrao_local, &desvio_padrao_global, 1, MPI_FLOAT, MPI_SUM, 0, MPI_COMM_WORLD);
-
-    // Processo 0 imprime o resultado
+    
     if (rank == 0) {
-        desvio_padrao_global = sqrt(desvio_padrao_global / n);
-        printf("Desvio Padrão: %.6f\n", desvio_padrao_global);
+        // Imprime o desvio padrão global
+        desvio_padrao_global = sqrt(desvio_padrao_global / MAX);
+        printf("Desvio padrão: %.2f\n", desvio_padrao_global);
     }
-
-    /*
-    FIM
-    */
-
-    free(vetor);
+    
     MPI_Finalize();
     return 0;
 }
@@ -85,4 +74,20 @@ float *gerar_vetor(int n) {
         vetor[i] = num;
     }
     return vetor;
+}
+
+float calcular_media(float *vetor, int n) {
+    float soma = 0.0;
+    for (int i = 0; i < n; i++) {
+        soma += vetor[i];
+    }
+    return soma / n;
+}
+
+float calcular_desvio_padrao(float *vetor, int n, float media) {
+    float soma = 0.0;
+    for (int i = 0; i < n; i++) {
+        soma += pow(vetor[i] - media, 2);
+    }
+    return soma;
 }
